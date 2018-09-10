b37f9a4a61b3bbd954f1997881c1f79d73457fd8
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-10 14:09:32 -0600

    This is for the chapter two assignment!

---
+++ src/AlannaChapter2Prog2/Chapter2Prog2.java
@@ -0,0 +1,17 @@
+package AlannaChapter2Prog2;
+/*
+ * Alanna Botscharow
+ * 9/18
+ * Chapter 2 program2 
+ */
+public class Chapter2Prog2 {
+	public static void main(String[] args) {
+		// displays five messages.
+		System.out.println("Welome to Java");
+		System.out.println("Welome to Java");
+		System.out.println("Welome to Java");
+		System.out.println("Welome to Java");
+		System.out.println("Welome to Java");
+		
+	}
+}
